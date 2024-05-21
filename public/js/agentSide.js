def7fcec40d54a7a07afd8acecbf9c1ae85ecf9a
Abhishek Tyagi <abhishektyagi@gmail.com>
2024-05-21 18:17:48 +0530

    default build like a chat bot

--- public/js/agentSide.js
+++ public/js/agentSide.js
@@ -25,111 +25,133 @@ Socket.on('get-receiver-id', (data) => {
     receiver = data.receiver;
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const text = document.querySelectorAll(".text");
-    const message = document.querySelector(".message");
-    const chatContainer = document.querySelector(".chat-texts");
-    const sendMessage = document.querySelector(".send-message-button");
 
-    sendMessage.addEventListener("click", (e) => {
+const text = document.querySelectorAll(".text");
+const message = document.querySelector(".message");
+const chatContainer = document.querySelector(".chat-texts");
+const sendMessage = document.querySelector(".send-message-button");
 
-        if (message.value && queryId !== null && agentId !== null) {
+sendMessage.addEventListener("click", (e) => {
 
-            appendMessages('sender', message.value);
-            chatContainer.scrollTop = chatContainer.scrollHeight;
+    if (message.value && queryId !== null && agentId !== null) {
 
-            if (chatId !== '' && receiver !== '') {
-                console.log(receiver);
-                console.log('Sending message');
-                Socket.emit('message-send', { receiver: receiver, queryId: queryId, message: message.value, sender: queryId });
-            }
-            else {
-                console.log(chatId, receiver);
-                if (queryId !== null && agentId !== null) {
-                    console.log('Broadcasting message');
-                    Socket.emit('auto-assign', { agentId: agentId, queryId: queryId, message: message.value, time: new Date().toLocaleString() });
-                }
-                else {
-                    // console.log('Error in broadcasting message');
-                }
-            }
-            message.value = "";
-        }
+        appendMessages('sender', message.value);
+        chatContainer.scrollTop = chatContainer.scrollHeight;
 
-    });
-    function appendMessages(sender, message) {
-        if (sender === 'sender') {
-            let text = document.createElement("div");
-            let profilePicContainer = document.createElement("div");
-            let pic = document.createElement("img");
-            let textContent = document.createElement("div");
-            let timeStamp = document.createElement("span");
-            let name = document.createElement("h5");
-            let date = new Date();
-            let hours = date.getHours();
-            let minutes = date.getMinutes() + "";
-            let time = `${hours}:${minutes.padStart(2, "0")}hrs`;
-            name.innerText = agentId;
-            timeStamp.classList.add("timestamp");
-            timeStamp.innerText = time;
-            textContent.classList.add("text-content");
-            textContent.appendChild(name);
-            textContent.append(message);
-            textContent.appendChild(timeStamp);
-            pic.setAttribute("src", "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80");
-            text.classList.add("text");
-            text.classList.add("sent");
-            profilePicContainer.classList.add("profile-pic");
-            profilePicContainer.appendChild(pic);
-            text.appendChild(profilePicContainer);
-            text.appendChild(textContent);
-            chatContainer.appendChild(text);
-            chatContainer.scrollTop = chatContainer.scrollHeight;
+        if (chatId !== '' && receiver !== '') {
+            console.log(receiver);
+            console.log('Sending message');
+            Socket.emit('message-send', { receiver: receiver, queryId: queryId, message: message.value, sender: queryId });
         }
         else {
-            let text = document.createElement("div");
-            let profilePicContainer = document.createElement("div");
-            let pic = document.createElement("img");
-            let textContent = document.createElement("div");
-            let timeStamp = document.createElement("span");
-            let name = document.createElement("h5");
-            let date = new Date();
-            let hours = date.getHours();
-            let minutes = date.getMinutes() + "";
-            let time = `${hours}:${minutes.padStart(2, "0")}hrs`;
-            name.innerText = 'Culture Support';
-            timeStamp.classList.add("timestamp");
-            timeStamp.innerText = time;
-            textContent.classList.add("text-content");
-            textContent.appendChild(name);
-            textContent.append(message);
-            textContent.appendChild(timeStamp);
-            pic.setAttribute("src", "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80");
-            text.classList.add("text");
-            profilePicContainer.classList.add("profile-pic");
-            profilePicContainer.appendChild(pic);
-            text.appendChild(profilePicContainer);
-            text.appendChild(textContent);
-            chatContainer.appendChild(text);
-            chatContainer.scrollTop = chatContainer.scrollHeight;
+            console.log(chatId, receiver);
+            if (queryId !== null && agentId !== null) {
+                console.log('Broadcasting message');
+                Socket.emit('auto-assign', { agentId: agentId, queryId: queryId, message: message.value, time: new Date().toLocaleString() });
+            }
+            else {
+                // console.log('Error in broadcasting message');
+            }
         }
+        message.value = "";
     }
 
-    Socket.on('get-individual-chat', (data) => {
-        console.log('Individual Chat:', data);
-        const chats = data.chat;
-        chats.forEach((message) => {
-            const sender = (message.sender === queryId) ? 'sender' : 'receiver';
-            appendMessages(sender, message.message);
-        });
-    });
+});
 
-    Socket.on('receive-message', (data) => {
-        console.log('Message:', data);
-        const chats = data.Chat;
-        chats.forEach((message) => {
-            const sender = (message.sender === queryId) ? 'sender' : 'receiver';
-            appendMessages(sender, message.message);
-        });
+function appendMessages(sender, message) {
+    if (sender === 'sender') {
+        let text = document.createElement("div");
+        let profilePicContainer = document.createElement("div");
+        let pic = document.createElement("img");
+        let textContent = document.createElement("div");
+        let timeStamp = document.createElement("span");
+        let name = document.createElement("h5");
+        let date = new Date();
+        let hours = date.getHours();
+        let minutes = date.getMinutes() + "";
+        let time = `${hours}:${minutes.padStart(2, "0")}hrs`;
+        name.innerText = agentId;
+        timeStamp.classList.add("timestamp");
+        timeStamp.innerText = time;
+        textContent.classList.add("text-content");
+        textContent.appendChild(name);
+        textContent.append(message);
+        textContent.appendChild(timeStamp);
+        pic.setAttribute("src", "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80");
+        text.classList.add("text");
+        text.classList.add("sent");
+        profilePicContainer.classList.add("profile-pic");
+        profilePicContainer.appendChild(pic);
+        text.appendChild(profilePicContainer);
+        text.appendChild(textContent);
+        chatContainer.appendChild(text);
+        chatContainer.scrollTop = chatContainer.scrollHeight;
+    }
+    else {
+        let text = document.createElement("div");
+        let profilePicContainer = document.createElement("div");
+        let pic = document.createElement("img");
+        let textContent = document.createElement("div");
+        let timeStamp = document.createElement("span");
+        let name = document.createElement("h5");
+        let date = new Date();
+        let hours = date.getHours();
+        let minutes = date.getMinutes() + "";
+        let time = `${hours}:${minutes.padStart(2, "0")}hrs`;
+        name.innerText = 'Culture Support';
+        timeStamp.classList.add("timestamp");
+        timeStamp.innerText = time;
+        textContent.classList.add("text-content");
+        textContent.appendChild(name);
+        textContent.append(message);
+        textContent.appendChild(timeStamp);
+        pic.setAttribute("src", "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80");
+        text.classList.add("text");
+        profilePicContainer.classList.add("profile-pic");
+        profilePicContainer.appendChild(pic);
+        text.appendChild(profilePicContainer);
+        text.appendChild(textContent);
+        chatContainer.appendChild(text);
+        chatContainer.scrollTop = chatContainer.scrollHeight;
+    }
+}
+
+function handleOption(option) {
+    let message;
+    switch (option) {
+        case 'booking':
+            message = 'Searching your booking Information...';
+            break;
+        case 'travelerName':
+            message = 'Searching traveler name...';
+            break;
+        case 'chat':
+            message = 'Starting chat...';
+            break;
+        default:
+            console.log('Unknown option selected');
+            return;
+    }
+    appendMessages('sender', option);
+    setTimeout(() => {
+        appendMessages('receiver', message);
+    }, 1000);
+}
+
+Socket.on('get-individual-chat', (data) => {
+    console.log('Individual Chat:', data);
+    const chats = data.chat;
+    chats.forEach((message) => {
+        const sender = (message.sender === queryId) ? 'sender' : 'receiver';
+        appendMessages(sender, message.message);
+    });
+});
+
+Socket.on('receive-message', (data) => {
+    console.log('Message:', data);
+    const chats = data.Chat;
+    chats.forEach((message) => {
+        const sender = (message.sender === queryId) ? 'sender' : 'receiver';
+        appendMessages(sender, message.message);
     });
 });
